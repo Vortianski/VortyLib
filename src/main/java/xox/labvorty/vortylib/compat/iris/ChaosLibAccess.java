a0a9ec554c2c4c61036d16d5cb2ac8f849c652c1
Vortianski <vortianski@gmail.com>
2026-09-02 14:25:00 +0300

    workflow error fix

--- src/main/java/xox/labvorty/vortylib/compat/iris/ChaosLibAccess.java
+++ src/main/java/xox/labvorty/vortylib/compat/iris/ChaosLibAccess.java
@@ -1,13 +1,51 @@
 package xox.labvorty.vortylib.compat.iris;
 
-import dev.elifian.chaoslib.api.iris.IrisCompat;
 import net.minecraft.client.renderer.RenderType;
 
+import java.lang.reflect.Method;
+
 public final class ChaosLibAccess {
-        private ChaosLibAccess() {
+
+    private static final Method WRAP_ENTITY_RENDER_LAYER;
+
+    static {
+        Method method = null;
+
+        try {
+            Class<?> irisCompat = Class.forName(
+                    "dev.elifian.chaoslib.api.iris.IrisCompat"
+            );
+
+            method = irisCompat.getMethod(
+                    "wrapEntityRenderLayer",
+                    RenderType.class,
+                    RenderType.class
+            );
+        } catch (Throwable ignored) {
         }
 
-        public static RenderType wrapEntityRenderLayer(RenderType renderLayer, RenderType shadowRenderLayer) {
-            return IrisCompat.wrapEntityRenderLayer(renderLayer, shadowRenderLayer);
+        WRAP_ENTITY_RENDER_LAYER = method;
+    }
+
+    private ChaosLibAccess() {
+    }
+
+    public static RenderType wrapEntityRenderLayer(
+            RenderType renderLayer,
+            RenderType shadowRenderLayer
+    ) {
+        if (WRAP_ENTITY_RENDER_LAYER == null) {
+            return renderLayer;
         }
+
+        try {
+            return (RenderType) WRAP_ENTITY_RENDER_LAYER.invoke(
+                    null,
+                    renderLayer,
+                    shadowRenderLayer
+            );
+        } catch (Throwable ignored) {
+            return renderLayer;
+        }
+    }
 }
